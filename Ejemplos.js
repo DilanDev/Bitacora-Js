ad0936fd9c9116ce561f863e004f1dde1f2e29c6
DilanDev <dilan02ospino@gmail.com>
2025-04-21 15:17:47 -0500

    21.04.2025

---
+++ Ejemplos.js
@@ -0,0 +1,14 @@
+// variable
+let nombre = 'Dilan';
+
+// tipos de datos
+let apellido = 'ospino';
+let valor = 23;
+let money = false;
+let dev = null;
+let color;
+
+//Promblema con interaciones
+/*crea una pagina wed que pida el nombre y lo muestre en pantalla*/
+let name = prompt("¿Como te llamas?");
+alert(`Holaa ${name}`);
